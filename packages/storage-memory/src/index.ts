cc8aefd280f0c0064345bc98312e1ad94f20c5b2
alkeicam <maciej.grula@gmail.com>
2025-04-30 11:56:48 +0200

    adding capability to clone cache items when retrieving from cache.

--- packages/storage-memory/src/index.ts
+++ packages/storage-memory/src/index.ts
@@ -1,12 +1,25 @@
 import { ICacheItem, IStorage } from "node-ts-cache"
+import { IMemoryCacheOptions } from "./memory-types"
+export * from "./memory-types"
 
 export class MemoryStorage implements IStorage {
     private memCache: any = {}
+    private shouldClone: boolean = false; // by default no cloning (default behaviour)
 
-    constructor() {}
+    constructor(options?:IMemoryCacheOptions) {
+        if(options){
+            this.shouldClone = options.clone
+        }
+    }
 
     public async getItem(key: string): Promise<ICacheItem | undefined> {
-        return this.memCache[key]
+        let response;
+        if(this.shouldClone){
+            response = JSON.parse(JSON.stringify(this.memCache[key]));
+        }else{
+            response = this.memCache[key]
+        }
+        return response;
     }
 
     public async setItem(key: string, content: any): Promise<void> {
---
+++ packages/storage-memory/src/memory-types.ts
@@ -0,0 +1,3 @@
+export interface IMemoryCacheOptions {
+    clone: boolean // when set to true data returned from cache will be deep copied (deep copy will be returned)
+}
